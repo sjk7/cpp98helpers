4e63c8ecb07f8e463996633ab273031e81ee97dd
secret <audioenhance@hotmail.com>
2020-04-15 17:57:33 +0100

    travis

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-set PATH=$MSBUILD_PATH:$PATH
+setx path /M "%path%;$MSBUILD_PATH"
 cmd.exe /c echo %PATH%
 cmd.exe /C MSBuild.exe /p:Platform=x86 /p:Configuration=Release /p:RunCodeAnalysis=False SPSCAudio.sln   
