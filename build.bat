c9c93fb9432108589c3155bec77f196c795cea5f
secret <audioenhance@hotmail.com>
2020-04-15 18:53:58 +0100

    yeh

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
-setx path /M "%path%;$MSBUILD_PATH"
-cmd.exe /c echo %PATH%
-cmd.exe /C MSBuild.exe /p:Platform=x86 /p:Configuration=Release /p:RunCodeAnalysis=False SPSCAudio.sln   
+rem cmd.exe /c echo %MSBUILD_PATH%
+cmd.exe /c "%MSBUILD_PATH%\MSBuild.exe" /p:Platform=x86 /p:Configuration=Release /p:RunCodeAnalysis=False SPSCAudio.sln 
