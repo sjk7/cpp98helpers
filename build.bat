66d093a4a3d9f495a0cc788afa3c76da530f9311
sjk7 <radiowebmasters@gmail.com>
2020-04-15 19:23:58 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-rem cmd.exe /c echo %MSBUILD_PATH%
-cmd.exe /c "%MSBUILD_PATH%\MSBuild.exe" /p:Platform=x86 /p:Configuration=Release /p:RunCodeAnalysis=False SPSCAudio.sln 
+setx path /M "$MSBUILD_PATH;%path%"
+cmd.exe /c "%MSBUILD_PATH%\MSBuild.exe" /p:Platform=x86 /p:Configuration=Release /p:RunCodeAnalysis=False SPSCAudio.sln
+
