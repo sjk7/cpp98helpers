3c16e8c20f022cbb738d9e88243049ceb8437ecf
s <radiowebmasters@gmail.com>
2020-04-19 09:47:39 +0100

    Added email notifications

---
+++ CMakeFiles/concurrent_tests.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/concurrent_tests.dir/SPSCAudio.cpp.obj"
+  "concurrent_tests.exe"
+  "concurrent_tests.exe.manifest"
+  "concurrent_tests.pdb"
+  "libconcurrent_tests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/concurrent_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/concurrent_tests.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for concurrent_tests.
+# This may be replaced when dependencies are built.
