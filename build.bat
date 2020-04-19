8760b0e5f42ef85c7d5b68f3064238f468fa9ee1
s <radiowebmasters@gmail.com>
2020-04-19 09:27:38 +0100

    Adding cmake for MINGW

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 setx path /M "%path%;$MSBUILD_PATH"
 cmd.exe /c echo %PATH%
-cmd.exe /C MSBuild.exe /p:Platform=x86 /p:Configuration=Release /p:RunCodeAnalysis=False SPSCAudio.sln 
+cmd.exe /C MSBuild.exe /p:Platform=x86 /p:Configuration=Debug /p:RunCodeAnalysis=False SPSCAudio.sln 
